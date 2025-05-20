b3bc23e8ce1ff07fa4ca28f6e13295cfd0ac10ef
NotRuged <andre_k@gmx.at>
2025-05-20 10:29:57 +0200

    Updated Version

--- MVVM_Einheitenumrechner/obj/Debug/net8.0-windows/App.g.cs
+++ MVVM_Einheitenumrechner/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace MVVM_Einheitenumrechner {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace MVVM_Einheitenumrechner {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.3.0")]
         public static void Main() {
             MVVM_Einheitenumrechner.App app = new MVVM_Einheitenumrechner.App();
             app.InitializeComponent();
